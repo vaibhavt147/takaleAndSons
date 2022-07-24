2315bdf460730eb11464080fb4e5ff50f0892a7b
vaibhavt147 <vaibhavt147@gmail.com>
2022-07-24 12:30:56 +0530

    Adding Newsletter form to collect user's information

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
 app.set('port',process.env.PORT || 3000);
 
-app.use(express.static(__dirname + '/public'))
-
+app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
 app.get('/', function(req,res){
     res.render('home')
 })
 
 app.get('/about', function(req,res){
     res.render('about', {fortune: fortune.getFortune()});
+})
+
+app.get('/newsletter', function(req,res){
+    res.render('newsletter', {csrf: 'CSRF toekn goes here'});
+})
+
+app.post('/process', function(req,res){
+    console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF toekn (from hidden form field): '+ req.body._csrf);
+    console.log('Name (from visible form field): '+ req.body.name);
+    console.log('Email (from visible form field): '+ req.body.email);
+    res.redirect(303, '/thank-you')
 })
 
 //custom 404 page
